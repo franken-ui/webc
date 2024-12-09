16d86a0b058bbd7c3d273fa52d21f6d19da1d9b2
Sveltecult <sveltecult@proton.me>
2024-12-09 09:49:45 +0800

    add cls-custom attribute to input-pin

--- src/js/components/input-pin.ts
+++ src/js/components/input-pin.ts
@@ -1,16 +1,21 @@
 import { LitElement, PropertyValues, html } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
+import { parseOptions } from '../helpers/common';
+
+type Cls = {
+  div: string;
+};
 
 @customElement('uk-input-pin')
 export class InputPin extends LitElement {
   @property({ type: Boolean })
   autofocus: boolean = false;
 
-  @property({ type: Boolean })
-  disabled: boolean = false;
+  @property({ type: String })
+  'cls-custom': string = '';
 
   @property({ type: Boolean })
-  error: boolean = false;
+  disabled: boolean = false;
 
   @property({ type: Number })
   length: number = 6;
@@ -18,8 +23,10 @@ export class InputPin extends LitElement {
   @property({ type: String })
   name: string = '';
 
-  @property({ type: Boolean })
-  separated: boolean = false;
+  @state()
+  $cls: Cls = {
+    div: '',
+  };
 
   @state()
   $focus: undefined | number;
@@ -30,7 +37,21 @@ export class InputPin extends LitElement {
   connectedCallback(): void {
     super.connectedCallback();
 
-    this.removeAttribute('uk-cloak');
+    if (this['cls-custom']) {
+      const cls = parseOptions(this['cls-custom']) as Cls | string;
+
+      if (typeof cls === 'string') {
+        this.$cls['div'] = cls;
+      } else {
+        Object.keys(this.$cls).forEach(a => {
+          const key = a as 'div';
+
+          if (cls[key]) {
+            this.$cls[key] = cls[key];
+          }
+        });
+      }
+    }
   }
 
   protected createRenderRoot(): HTMLElement | DocumentFragment {
@@ -54,12 +75,9 @@ export class InputPin extends LitElement {
   render() {
     return html`
       <div
-        class="uk-input-pin ${this.separated === true
-          ? 'uk-input-pin-separated'
-          : ''} ${this.disabled === true ? 'uk-disabled' : ''} ${this.error ===
-        true
-          ? 'uk-form-danger'
-          : ''}"
+        class="uk-input-pin ${this.disabled === true
+          ? 'uk-disabled'
+          : ''} ${this.$cls['div']}"
       >
         ${Array(this.length)
           .fill('')
@@ -126,7 +144,11 @@ export class InputPin extends LitElement {
               />`,
           )}
       </div>
-      <input type="hidden" name="${this.name}" .value=${this.$value} />
+      ${this.name
+        ? html`
+            <input type="hidden" name="${this.name}" .value=${this.$value} />
+          `
+        : ''}
     `;
   }
 }
